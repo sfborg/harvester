2c839baa68665f663767ffb8e68d15975ecd2ae8
Dmitry Mozzherin <dmozzherin@gmail.com>
2025-02-26 13:16:51 +0000

    use iterator for ion names

--- internal/io/ion/name.go
+++ internal/io/ion/name.go
@@ -11,6 +11,38 @@ import (
 	"github.com/gnames/coldp/ent/coldp"
 )
 
+// nameIterator returns an iterator function that yields coldp.Name
+// structs from a TSV file scanner.
+func nameIterator(scanner *bufio.Scanner) func(func(coldp.Name) bool) {
+	return func(yield func(coldp.Name) bool) {
+		fields := struct {
+			id, name, authorship int
+		}{
+			id:         0,
+			name:       3,
+			authorship: 4,
+		}
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			row := strings.Split(line, "\t")
+			n := coldp.Name{
+				ID:                   row[fields.id],
+				ScientificName:       row[fields.name],
+				Authorship:           row[fields.authorship],
+				ScientificNameString: row[fields.name] + " " + row[fields.authorship],
+			}
+			if !yield(n) {
+				return
+			}
+		}
+	}
+}
+
+// importNames reads names from a TSV file and processes them in batches.
+// It uses a scanner to read the file line by line and an iterator function
+// to yield coldp.Name structs. The names are processed in batches of size
+// specified in the configuration.
 func (i *ion) importNames() error {
 	f, err := os.Open(filepath.Join(i.cfg.ExtractDir, "ion.tsv"))
 	if err != nil {
@@ -20,52 +52,47 @@ func (i *ion) importNames() error {
 
 	scanner := bufio.NewScanner(f)
 
-	// skip headers
-	scanner.Scan()
-
-	fld := struct {
-		id, name, authorship int
-	}{
-		id:         0,
-		name:       3,
-		authorship: 4,
+	// Skip the header row.
+	if !scanner.Scan() {
+		return scanner.Err()
 	}
+
+	iter := nameIterator(scanner)
+
 	var count int
 	names := make([]coldp.Name, 0, i.cfg.BatchSize)
-	for scanner.Scan() {
+
+	for n := range iter {
 		count++
-		line := scanner.Text()
-		row := strings.Split(line, "\t")
-		n := coldp.Name{
-			ID:                   row[fld.id],
-			ScientificName:       row[fld.name],
-			Authorship:           row[fld.authorship],
-			ScientificNameString: row[fld.name] + " " + row[fld.authorship],
-		}
 		names = append(names, n)
-		if count%i.cfg.BatchSize == 0 {
-			fmt.Fprint(os.Stderr, "\r", strings.Repeat(" ", 80))
-			fmt.Fprintf(os.Stderr, "\rProcessed %s lines", humanize.Comma(int64(count)))
-			err = i.sfga.InsertNames(names)
-			if err != nil {
+		if len(names) == i.cfg.BatchSize {
+			if err := processBatch(i, names, count); err != nil {
 				return err
 			}
 			names = names[:0]
 		}
 	}
+
+	if len(names) > 0 {
+		if err := processBatch(i, names, count); err != nil {
+			return err
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func processBatch(i *ion, names []coldp.Name, count int) error {
 	fmt.Fprint(os.Stderr, "\r", strings.Repeat(" ", 80))
 	fmt.Fprintf(os.Stderr, "\rProcessed %s lines", humanize.Comma(int64(count)))
-	if len(names) == 0 {
-		return nil
-	}
+	err := i.sfga.InsertNames(names)
 
-	if err = i.sfga.InsertNames(names); err != nil {
+	if err != nil {
 		return err
 	}
-
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-
 	return nil
 }
